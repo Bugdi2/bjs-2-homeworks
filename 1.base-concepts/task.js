e15496ee94ed76a25656f75bbd0fc95fc62abdec
Bugdi <bagdi2@mail.ru>
2022-11-30 16:56:43 +0300

    HomeWork2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -27,5 +27,20 @@ solveEquation(1, 2, 1)
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let body;
+  let monthsPers;
+  let payMonth;
+  let sum;
+  if( isNaN(percent) || isNaN(contribution) || isNaN(amount)|| isNaN(countMonths)){
+    console.log('Введите корректные данные');
+  }
+
+  body = amount - contribution;
+  monthsPers = percent/100/countMonths;
+  payMonth = body * (monthsPers + (monthsPers/ (((1 + monthsPers)**countMonths)-1)))
+  sum = Number((payMonth * countMonths).toFixed(2));
+  console.log(sum);
+  return sum;
 }
+
+calculateTotalMortgage(10, 0, 50000, 12)
