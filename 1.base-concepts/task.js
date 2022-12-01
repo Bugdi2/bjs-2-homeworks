36c5d4b62f78fc2dc0f69e7d234dc8dc5dac2177
Bugdi <bagdi2@mail.ru>
2022-12-01 23:44:46 +0300

    Fixed bags

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,20 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
+  const arr = [];
   let sqrtOne;
   let sqrtTwo;
-  let D = Math.pow(b, 2) - 4*a*c;
+  let discriminant = Math.pow(b, 2) - 4*a*c;
 
-  if (D < 0){
+  if (discriminant < 0){
     console.log ('Корней нет!')
-  } else if (D === 0) {
+  } else if (discriminant === 0) {
     sqrtOne = -b/(2*a);
     arr.push(sqrtOne);
     console.log(arr);
   } else {
-    sqrtOne= (-b + Math.sqrt(D))/ (2*a);
-    sqrtTwo = (-b - Math.sqrt(D))/ (2*a);
+    sqrtOne= (-b + Math.sqrt(discriminant))/ (2*a);
+    sqrtTwo = (-b - Math.sqrt(discriminant))/ (2*a);
     arr.push(sqrtOne);
     arr.push(sqrtTwo);
     console.log('Первый корень=' + arr[0] + ';' + ' Второй корень=' + arr[1])
@@ -33,14 +33,16 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let sum;
   if( isNaN(percent) || isNaN(contribution) || isNaN(amount)|| isNaN(countMonths)){
     console.log('Введите корректные данные');
+    return false;
+
   }
 
   body = amount - contribution;
-  monthsPers = percent/100/countMonths;
+  monthsPers = percent/100/12;
   payMonth = body * (monthsPers + (monthsPers/ (((1 + monthsPers)**countMonths)-1)))
   sum = Number((payMonth * countMonths).toFixed(2));
   console.log(sum);
   return sum;
 }
 
-calculateTotalMortgage(10, 0, 50000, 12)
+calculateTotalMortgage("2", 0, 50000, 12)
