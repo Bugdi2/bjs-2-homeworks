2fdf08a83592b96c88eb66664d6e5609c57871d3
Bugdi <bagdi2@mail.ru>
2023-01-09 16:59:30 +0300

    first commit

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,8 +1,27 @@
 class AlarmClock{
     constructor(){
-        alarmCollection = [];
-        intervalId;
+        this.alarmCollection = [{
+            time: "00:00",
+            callback: () => console.log ('звонок'),
+            canCall: true,
+
+        }];
+        this.intervalId === null;
     }
 
-    addClock ()
+    addClock (time, callback){
+        if (!time || !callback){
+            throw new Error('Отсутствуют обязательные аргументы')
+        }
+        
+        if (this.alarmCollection.some(alarm => alarm.time === time)){
+            console.warn('Уже присутствует звонок на это же время')
+        }
+
+        this.alarmCollection.push({time, callback, canCall: true });
+    }
+
+    removeClock(time){
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time)
+    }
 }
